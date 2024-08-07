c651766f10cea4e974085ca3ada7a82df89285d5
Maxwell <xj.master@gmail.com>
2024-08-07 23:16:33 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import fs from "fs";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    https: {
-      key: fs.readFileSync("C:\\192.168.0.104-key.pem"),
-      cert: fs.readFileSync("C:\\192.168.0.104.pem"),
-    },
   },
   plugins: [react()],
 });
